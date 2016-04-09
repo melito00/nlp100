1c16703099c22aae62e8cfa6be64fee0eb4dba94
Kenji Yamada <kenji.yamada@gmail.com>
2016-04-09 15:17:09 +0100

    initial code

--- src/main/scala/Nlp100.scala
+++ src/main/scala/Nlp100.scala
@@ -1,6 +1,21 @@
 // package mobi.jnkymd.nlp100
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.stream.Stream
+import java.util.function._
+import java.nio.charset.StandardCharsets
+import java.io.IOException
 
 object Nlp100 {
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
+    val file = "file.txt"
+    try {
+      val stream = Files.lines(Paths.get(file), StandardCharsets.UTF_8)
+      stream.forEach(new Consumer[String] {
+        override def accept(t: String): Unit = println(t)
+      })
+    } catch {
+      case e: IOException => e.printStackTrace()
+    }
   }
 }
